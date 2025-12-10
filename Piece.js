6e57da5ed8f01034151f4999081199e5ee68cf67
Marius DARRAS <mdarras7@myges.fr>
2025-12-10 15:25:13 +0100

    Création de .\Piece.js pour gérer les pièces

---
+++ Piece.js
@@ -0,0 +1,29 @@
+class Piece {
+  constructor(name) {
+    this.name = name;
+    this.sensors = [];
+    this.observers = [];
+  }
+
+  addSensor(sensor) {
+    this.sensors.push(sensor);
+  }
+
+  addNotif(notif) {
+    this.notif.push(notif);
+  }
+
+  checkSensors() {
+    for (const sensor of this.sensors) {
+      const alertMessage = sensor.detect();
+      this.notifierAll(alertMessage);
+    }
+  }
+
+  notifierAll(message) {
+    const fullMessage = this.name + " : " + message;
+    for (const observer of this.observers) {
+      observer.send(fullMessage);
+    }
+  }
+}
